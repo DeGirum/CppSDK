9413c4a87417755d25045a1dc211186b448f7ae4
degirumautomation <main.office@degirum.com>
2022-08-03 00:27:54 +0000

    C++ SDK release 0.3.2

--- README.md
+++
@@ -1,6 +0,0 @@
-# DeGirum C++ SDK
-
-This repo contains source code of DeGirum C++ SDK.
-C++ SDK is used to perfrom AI model inferences on DeGirum AI servers.
-
-Please visit [DeGirum Developers Center](https://degirum.github.io/) for more information on DeGirum AI server installation and configuration.
--- inc/Utilities/dg_version.h
+++ inc/Utilities/dg_version.h
@@ -21,16 +21,16 @@
 	#define DG_VERSION_MINOR 3
 #endif
 #ifndef DG_VERSION_REVISION
-	#define DG_VERSION_REVISION 1
+	#define DG_VERSION_REVISION 2
 #endif
 #ifndef DG_GIT_REV
-	#define DG_GIT_REV 	"87d401b"
+	#define DG_GIT_REV 	"37870e9"
 #endif
 #ifndef DG_GIT_TAG
 	#define DG_GIT_TAG 	""
 #endif
 #ifndef DG_GIT_BRANCH
-	#define DG_GIT_BRANCH 	"0.3.1"
+	#define DG_GIT_BRANCH 	"0.3.2"
 #endif
 
 #endif // DG_GLOBAL_VERSION_H
