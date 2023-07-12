ef45c10398ab71369981a85dc07d8dd24afdbe5d
degirumautomation <main.office@degirum.com>
2023-07-12 21:32:34 +0000

    C++ SDK release 0.8.4

--- inc/Utilities/dg_version.h
+++ inc/Utilities/dg_version.h
@@ -21,16 +21,16 @@
 	#define DG_VERSION_MINOR 8
 #endif
 #ifndef DG_VERSION_REVISION
-	#define DG_VERSION_REVISION 3
+	#define DG_VERSION_REVISION 4
 #endif
 #ifndef DG_GIT_REV
-	#define DG_GIT_REV 	"745514b"
+	#define DG_GIT_REV 	"5ce7fef"
 #endif
 #ifndef DG_GIT_TAG
 	#define DG_GIT_TAG 	""
 #endif
 #ifndef DG_GIT_BRANCH
-	#define DG_GIT_BRANCH 	"0.8.3"
+	#define DG_GIT_BRANCH 	"0.8.4"
 #endif
 
 #endif // DG_GLOBAL_VERSION_H
