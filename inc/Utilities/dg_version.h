927c04badc0aa99134aa9cfea659a8097033cf33
vlad-nn <78237151+vlad-nn@users.noreply.github.com>
2023-07-27 12:00:06 -0700

    C++ SDK release 0.9.0 (#16)
    Co-authored-by: degirumautomation <main.office@degirum.com>

--- inc/Utilities/dg_version.h
+++ inc/Utilities/dg_version.h
@@ -18,19 +18,19 @@
 	#define DG_VERSION_MAJOR 0
 #endif
 #ifndef DG_VERSION_MINOR
-	#define DG_VERSION_MINOR 8
+	#define DG_VERSION_MINOR 9
 #endif
 #ifndef DG_VERSION_REVISION
-	#define DG_VERSION_REVISION 3
+	#define DG_VERSION_REVISION 0
 #endif
 #ifndef DG_GIT_REV
-	#define DG_GIT_REV 	"745514b"
+	#define DG_GIT_REV 	"bc47492"
 #endif
 #ifndef DG_GIT_TAG
 	#define DG_GIT_TAG 	""
 #endif
 #ifndef DG_GIT_BRANCH
-	#define DG_GIT_BRANCH 	"0.8.3"
+	#define DG_GIT_BRANCH 	"0.9.0"
 #endif
 
 #endif // DG_GLOBAL_VERSION_H
